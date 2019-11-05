094c35dc2e90dc76ed51961dfa7614cfb0f88a8d
pengzhimingcn@163.com <pzmcn163.com.cnes>
2019-11-05 13:03:34 +0800

    二维码异常捕捉

--- kera/src/main/java/com/sdk/Qr_code/code/CameraConfigurationManager.java
+++ kera/src/main/java/com/sdk/Qr_code/code/CameraConfigurationManager.java
@@ -53,24 +53,26 @@ private static final int DESIRED_SHARPNESS = 30;
    * Reads, one time, values from the camera that are needed by the app.
    */
   public void initFromCameraParameters(Camera camera) {
-    Camera.Parameters parameters = camera.getParameters();
-    previewFormat = parameters.getPreviewFormat();
-    previewFormatString = parameters.get("preview-format");
-    Log.d(TAG, "Default preview format: " + previewFormat + '/' + previewFormatString);
-    WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-    Display display = manager.getDefaultDisplay();
-    screenResolution = new Point(display.getWidth(), display.getHeight());
-    Log.d(TAG, "Screen resolution: " + screenResolution);
-    Point screenResolutionForCamera = new Point();
-    screenResolutionForCamera.x = screenResolution.x;
-    screenResolutionForCamera.y = screenResolution.y;
-    // preview size is always something like 480*320, other 320*480
-	if (screenResolution.x < screenResolution.y) {
-		screenResolutionForCamera.x = screenResolution.y;
-		screenResolutionForCamera.y = screenResolution.x;
-	}
-	cameraResolution = getCameraResolution(parameters, screenResolutionForCamera);
-    Log.d(TAG, "Camera resolution: " + screenResolution);
+    try{
+      Camera.Parameters parameters = camera.getParameters();
+      previewFormat = parameters.getPreviewFormat();
+      previewFormatString = parameters.get("preview-format");
+      //Log.d(TAG, "Default preview format: " + previewFormat + '/' + previewFormatString);
+      WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+      Display display = manager.getDefaultDisplay();
+      screenResolution = new Point(display.getWidth(), display.getHeight());
+      //Log.d(TAG, "Screen resolution: " + screenResolution);
+      Point screenResolutionForCamera = new Point();
+      screenResolutionForCamera.x = screenResolution.x;
+      screenResolutionForCamera.y = screenResolution.y;
+      // preview size is always something like 480*320, other 320*480
+      if (screenResolution.x < screenResolution.y) {
+        screenResolutionForCamera.x = screenResolution.y;
+        screenResolutionForCamera.y = screenResolution.x;
+      }
+      cameraResolution = getCameraResolution(parameters, screenResolutionForCamera);
+      //Log.d(TAG, "Camera resolution: " + screenResolution);
+    }catch ( Exception e){e.printStackTrace();}
   }
 
   /**
--- kera/src/main/java/com/sdk/Qr_code/code/CaptureActivityHandler.java
+++ kera/src/main/java/com/sdk/Qr_code/code/CaptureActivityHandler.java
@@ -34,111 +34,119 @@ import com.sdk.Qr_code.manager.CameraManager;
 /**
  * This class handles all the messaging which comprises the state machine for
  * capture.
- * 
+ *
  * @author dswitkin@google.com (Daniel Switkin)
  */
 public final class CaptureActivityHandler extends Handler {
-	public static int auto_focus=10001;
-	public static int restart_preview=10002;
-	public static int decode_succeeded=10003;
-	public static int decode_failed=10004;
-	public static int decode=10005;
-	public static int return_scan_result=10006;
-	public static int launch_product_query=10007;
-	public static int quit2=10008;
-	private static final String TAG = CaptureActivityHandler.class
-			.getSimpleName();
+    public static int auto_focus = 10001;
+    public static int restart_preview = 10002;
+    public static int decode_succeeded = 10003;
+    public static int decode_failed = 10004;
+    public static int decode = 10005;
+    public static int return_scan_result = 10006;
+    public static int launch_product_query = 10007;
+    public static int quit2 = 10008;
+    private static final String TAG = CaptureActivityHandler.class
+            .getSimpleName();
 
-	private CaptureActivity activity = null;
-	private Activity activity2 = null;
-	private final DecodeThread decodeThread;// 解码线程
-	private State state;
+    private CaptureActivity activity = null;
+    private Activity activity2 = null;
+    private final DecodeThread decodeThread;// 解码线程
+    private State state;
 
-	// 枚举当前的状态类型
-	private enum State {
-		PREVIEW, // 预览
-		SUCCESS, // 成功
-		DONE// 完成
-	}
+    // 枚举当前的状态类型
+    private enum State {
+        PREVIEW, // 预览
+        SUCCESS, // 成功
+        DONE// 完成
+    }
 
-	public CaptureActivityHandler(CaptureActivity activity,
-			Vector<BarcodeFormat> decodeFormats, String characterSet) {
-		this.activity = activity;
-		decodeThread = new DecodeThread(activity, decodeFormats, characterSet,
-				new ViewfinderResultPointCallback(activity.getViewfinderView()));
-		decodeThread.start();
-		state = State.SUCCESS;
+    public CaptureActivityHandler(CaptureActivity activity,
+                                  Vector<BarcodeFormat> decodeFormats, String characterSet) {
+        this.activity = activity;
+        decodeThread = new DecodeThread(activity, decodeFormats, characterSet,
+                new ViewfinderResultPointCallback(activity.getViewfinderView()));
+        decodeThread.start();
+        state = State.SUCCESS;
 
-		// Start ourselves capturing previews and decoding.
-		CameraManager.get().startPreview();
-		restartPreviewAndDecode();
-	}
+        // Start ourselves capturing previews and decoding.
+        CameraManager.get().startPreview();
+        restartPreviewAndDecode();
+    }
 
-	@Override
-	public void handleMessage(Message message) {
-		int id = message.what;
-		if (id == auto_focus) {
-			if (state == State.PREVIEW) {
-				CameraManager.get().requestAutoFocus(this, auto_focus);
-			}
-		}
-		if (id == restart_preview) {
-			restartPreviewAndDecode();
-		}
-		if (id == decode_succeeded) {
-			state = State.SUCCESS;
-			Bundle bundle = message.getData();
-			Bitmap barcode = bundle == null ? null : (Bitmap) bundle
-					.getParcelable(DecodeThread.BARCODE_BITMAP);
-			activity.handleDecode((Result) message.obj, barcode);
-		}
-		if (id == decode_failed) {
-			state = State.PREVIEW;
-			CameraManager.get().requestPreviewFrame(decodeThread.getHandler(),
-					decode);
-		}
-		if (id == return_scan_result) {
-			if (activity != null) {
-				activity.setResult(Activity.RESULT_OK, (Intent) message.obj);
-				activity.finish();
-			}
-		}
-		if (id == launch_product_query) {
-			if (activity != null) {
-				String url = (String) message.obj;
-				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-				activity.startActivity(intent);
-			}
+    @Override
+    public void handleMessage(Message message) {
+        try {
+            int id = message.what;
+            if (id == auto_focus) {
+                if (state == State.PREVIEW) {
+                    CameraManager.get().requestAutoFocus(this, auto_focus);
+                }
+            }
+            if (id == restart_preview) {
+                restartPreviewAndDecode();
+            }
+            if (id == decode_succeeded) {
+                state = State.SUCCESS;
+                Bundle bundle = message.getData();
+                Bitmap barcode = bundle == null ? null : (Bitmap) bundle
+                        .getParcelable(DecodeThread.BARCODE_BITMAP);
+                activity.handleDecode((Result) message.obj, barcode);
+            }
+            if (id == decode_failed) {
+                state = State.PREVIEW;
+                CameraManager.get().requestPreviewFrame(decodeThread.getHandler(),
+                        decode);
+            }
+            if (id == return_scan_result) {
+                if (activity != null) {
+                    activity.setResult(Activity.RESULT_OK, (Intent) message.obj);
+                    activity.finish();
+                }
+            }
+            if (id == launch_product_query) {
+                if (activity != null) {
+                    String url = (String) message.obj;
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                    activity.startActivity(intent);
+                }
 
-		}
-	}
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	// 退出同步
-	public void quitSynchronously() {
-		state = State.DONE;
-		CameraManager.get().stopPreview();
-		Message quit = Message.obtain(decodeThread.getHandler(), quit2);
-		quit.sendToTarget();
-		try {
-			decodeThread.join();
-		} catch (InterruptedException e) {
-			// continue
-		}
+    // 退出同步
+    public void quitSynchronously() {
+        try{
+            state = State.DONE;
+            CameraManager.get().stopPreview();
+            Message quit = Message.obtain(decodeThread.getHandler(), quit2);
+            quit.sendToTarget();
+            try {
+                decodeThread.join();
+            } catch (InterruptedException e) {
+                // continue
+            }
 
-		// Be absolutely sure we don't send any queued up messages
-		removeMessages(decode_succeeded);
-		removeMessages(decode_failed);
-	}
+            // Be absolutely sure we don't send any queued up messages
+            removeMessages(decode_succeeded);
+            removeMessages(decode_failed);
+        }catch (Exception e){e.printStackTrace();}
+    }
 
-	private void restartPreviewAndDecode() {
-		if (state == State.SUCCESS) {
-			state = State.PREVIEW;
-			CameraManager.get().requestPreviewFrame(decodeThread.getHandler(),
-					decode);
-			CameraManager.get().requestAutoFocus(this, auto_focus);
-			activity.drawViewfinder();
-		}
-	}
+    private void restartPreviewAndDecode() {
+        try{
+            if (state == State.SUCCESS) {
+                state = State.PREVIEW;
+                CameraManager.get().requestPreviewFrame(decodeThread.getHandler(),
+                        decode);
+                CameraManager.get().requestAutoFocus(this, auto_focus);
+                activity.drawViewfinder();
+            }
+        }catch (Exception e){e.printStackTrace();}
+    }
 
 }
