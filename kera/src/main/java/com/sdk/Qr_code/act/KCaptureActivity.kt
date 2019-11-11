46b251c0b615314b90ec1fa11958e7875edfeae1
pengzhimingcn@163.com <pzmcn163.com.cnes>
2019-11-11 10:17:34 +0800

    二维码扫描结果

--- kera/src/main/java/com/sdk/Qr_code/act/KCaptureActivity.kt
+++ kera/src/main/java/com/sdk/Qr_code/act/KCaptureActivity.kt
@@ -134,7 +134,7 @@ open class KCaptureActivity : CaptureActivity() {
         super.onQrScanResult(str, bitmap)//父类的方法最好调用一下。
         //扫描框扫出来的二维码，bitmap一般都不为空；但是扫描本地图片则是空的。（因为扫描本地二维码时，没有传位图）
         //setResult(str)
-        KLoggerUtils.e("二维码扫描结果：\t" + str)
+        //KLoggerUtils.e("二维码扫描结果：\t" + str)
     }
 
     //本地二维码图片识别失败时回调。
