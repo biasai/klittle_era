d0ad96d4497f2d7e7b4cd6f8c26efe597ef918e9
pengzhimingcn@163.com <pzmcn163.com.cnes>
2019-11-15 16:15:59 +0800

    设置enable不可用样式属性；KVerticalLayout添加setEnabled()方法，可以控制所有子控件是否可用。

--- kera/src/main/kotlin/cn/oi/klittle/era/widget/viewpager/adapter/KFragmentStatePagerAdapter.kt
+++ kera/src/main/kotlin/cn/oi/klittle/era/widget/viewpager/adapter/KFragmentStatePagerAdapter.kt
@@ -21,6 +21,7 @@ import android.support.v4.app.FragmentStatePagerAdapter
 
 /**
  * FragmentStatePagerAdapter 适配器 fixme 这个adapter带有销毁功能,Fragment个数改变时，会自动刷新。亲测。效果杠杠的。
+ * fixme fragment每次切换时候，都会重新加载。即会重新执行 onCreateView()方法。
  */
 open class KFragmentStatePagerAdapter(fm: FragmentManager?, var fragments: MutableList<Fragment>) : FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
