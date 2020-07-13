f99be07ba6996e2ddad11b2045806092cc1e0c06
pengzhimingcn@163.com <pzmcn163.com.cnes>
2020-07-13 17:28:00 +0800

    ... Box2d物理引擎2.0.1引用。

---
+++ kera/src/main/kotlin/cn/oi/klittle/era/widget/gamepad/listener/JoystickListener.java
@@ -0,0 +1,10 @@
+package cn.oi.klittle.era.widget.gamepad.listener;
+
+import android.view.MotionEvent;
+
+/**
+ * 滚珠按键接口
+ */
+public interface JoystickListener {
+    public void onJoystikMotionEvent(MotionEvent event);
+}
