bb4d418627eb9cd06014e834a9a508803f08b283
pengzhimingcn@163.com <pzmcn163.com.cnes>
2020-08-14 08:30:06 +0800

    ...

--- kera/src/main/kotlin/cn/oi/klittle/era/widget/recycler/KGridLayoutManager.kt
+++ kera/src/main/kotlin/cn/oi/klittle/era/widget/recycler/KGridLayoutManager.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import java.lang.Exception
 
 /**
- * fixme 解决recyclerView刷新时异常；这是Google的天坑。是不是会报错。以下已经解决。
+ * fixme 解决recyclerView刷新时异常；这是Google的天坑。时不时会报错。以下已经解决。
  */
 open class KGridLayoutManager(context: Context?, spanCount: Int) : GridLayoutManager(context, spanCount) {
     override fun onLayoutChildren(recycler: RecyclerView.Recycler?, state: RecyclerView.State?) {
