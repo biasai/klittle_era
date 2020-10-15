46aea96039256b8f8fc369d81706fb79e3fc5dc3
pengzhimingcn@163.com <pzmcn163.com.cnes>
2020-10-15 10:37:10 +0800

    bubbles 气泡三角形独立分离。基本完成。

--- kera/src/main/kotlin/cn/oi/klittle/era/entity/widget/compat/KBubblesEntry.kt
+++ kera/src/main/kotlin/cn/oi/klittle/era/entity/widget/compat/KBubblesEntry.kt
@@ -6,26 +6,27 @@ import cn.oi.klittle.era.comm.kpx
 /**
  * 气泡三角实体类
  * @param direction 气泡的方向
- * @param xOffset 气泡的偏移量（整体偏移）；气泡三角，x轴偏移量，正数向右偏移，负数向左偏移
+ * @param xOffset 气泡的偏移量（整体偏移）；气泡三角，x轴偏移量，正数向右偏移，负数向左偏移；(fixme 即：正数向内偏移，负数向外偏移。)
  * @param yOffset 气泡三角，y轴偏移量；正数向下偏移，负数向上偏移。
  * @param bubblesOffset fixme airOffset是气泡顶点的偏移量，可以实现顶点倾斜效果。xOffset和yOffset是气泡整体的偏移量。
  * @param bubblesWidth 气泡的宽度
  * @param bubblesHeight 气泡的高度
  * @param all_radius 圆角(仅仅是气泡顶点的角度)
+ * @param bg_color 背景色
  * @param strokeWidth 边框的宽度
  * @param strokeColor 边框的颜色
- * @param bg_color 背景色
  * @param dashWidth 虚线长度（fixme 这里圆角和虚线一起使用；效果好像不是很好。）
  * @param dashGap 虚线之间的间隙
+ * @param isDrawRightStroke 是否绘制右边的边框（气泡顶点对应的底边）;默认不绘制。
  * @param isDraw 是否绘制
  * Created by 彭治铭 on 2019/4/18.
  */
 data class KBubblesEntry(var direction: Int = KBubblesEntry.DIRECTION_LEFT, var xOffset: Float = 0F, var yOffset: Float = 0F, var bubblesOffset: Float = 0f,
                          var bubblesWidth: Int = kpx.x(25), var bubblesHeight: Int = bubblesWidth,
-                         var all_radius: Float = 0F,
+                         var all_radius: Float = 0F, var bg_color: Int = Color.WHITE,
                          var strokeWidth: Float = 0F, var strokeColor: Int = Color.BLACK,
-                         var bg_color: Int = Color.WHITE,
                          var dashWidth: Float = 0F, var dashGap: Float = 0F,
+                         var isDrawRightStroke: Boolean = false,
                          var isDraw: Boolean = true) {
 
     companion object {
--- kera/src/main/kotlin/cn/oi/klittle/era/widget/compat/K6BubblesWidget.kt
+++ kera/src/main/kotlin/cn/oi/klittle/era/widget/compat/K6BubblesWidget.kt
@@ -6,43 +6,70 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import cn.oi.klittle.era.entity.widget.compat.KBubblesEntry
-//                    fixme 使用案例，仿对话框样式。
+
+//                    fixme 使用案例，仿对话框样式。贝塞尔曲线就不添加了。仿QQ曲线连接效果太难画了。直接用图片比较方便。(QQ用的也是图片)
+//                    fixme 左对话框 常态颜色（浅灰色）Color.parseColor("#F5F6FA")，按下颜色：Color.parseColor("#C4C5C8")
+//                    fixme 右对话框 常态颜色（浅蓝色）Color.parseColor("#00CAFC")，按下颜色：Color.parseColor("#00A2CA")
 //                    kview {
 //                        text = "我的名字叫诺亚方舟。哦"
 //                        gravity = Gravity.CENTER_VERTICAL
+//                        var mDirection = KBubblesEntry.DIRECTION_RIGHT//气泡方向
+//                        var mMargin = kpx.x(30)//radius外补丁
+//                        var norColor = Color.parseColor("#00CAFC")//常态颜色
+//                        var pressColor = Color.parseColor("#00A2CA")//按下颜色
 //                        bubbles {
-//                            direction = KAirEntry.DIRECTION_LEFT//fixme 气泡方向；如果是左方向，默认就是左垂直居中绘制。即：默认居中绘制。
+//                            direction = mDirection//fixme 气泡方向；如果是左方向，默认就是左垂直居中绘制。即：默认居中绘制。
 //                            bubblesWidth = kpx.x(20)
 //                            bubblesHeight = kpx.x(30)
-//                            bg_color = Color.parseColor("#00CAFC")
-//                            xOffset = kpx.x(11f)//气泡的偏移量（整体偏移）；正数向右偏移，负数向左偏移
-//                            yOffset = 0f
+//                            bg_color = norColor
+//                            if (direction == KBubblesEntry.DIRECTION_LEFT || direction == KBubblesEntry.DIRECTION_RIGHT) {
+//                                xOffset = kpx.x(11f)//气泡的偏移量（整体偏移）；正数向右偏移，负数向左偏移(fixme 即：正数向内偏移，负数向外偏移。)
+//                            } else {
+//                                yOffset = kpx.x(4f)
+//                            }
 //                            all_radius = 45f//气泡顶点的圆角角度（只对顶点有效）
 //                            bubblesOffset = -kpx.x(20f)//气泡顶点的偏移量
 //                        }
 //                        bubbles_press {
-//                            bg_color = Color.parseColor("#00A2CA")
+//                            bg_color = pressColor
 //                            all_radius = 0f
 //                            bubblesOffset = 0f
 //                            strokeColor = Color.RED
-//                            strokeWidth = kpx.x(3f)//fixme 边框，气泡最右边的边框是不会绘制。方便和radius组合对接。
-//                            xOffset = kpx.x(14f)
+//                            strokeWidth = kpx.x(3f)
+//                            isDrawRightStroke = false//fixme 是否绘制右边的边框（气泡顶点对应的底边）;默认不绘制。方便和radius组合对接。
+//                            if (direction == KBubblesEntry.DIRECTION_LEFT || direction == KBubblesEntry.DIRECTION_RIGHT) {
+//                                xOffset = kpx.x(14f)
+//                            } else {
+//                                yOffset = kpx.x(4f)
+//                            }
+//                            dashWidth=kpx.x(15f)
+//                            dashGap=kpx.x(10f)
 //                        }
 //                        radius {
-//                            all_radius(90)
-//                            bg_color = Color.parseColor("#00CAFC")
+//                            all_radius(45)
+//                            bg_color = norColor
 //                            strokeWidth = 0f
-//                            leftMargin = kpx.x(30)//左外补丁，控件边框的间距；fixme radius已经做了兼容处理，能够正常切割。
+//                            if (mDirection == KBubblesEntry.DIRECTION_LEFT) {
+//                                leftMargin = mMargin
+//                            } else if (mDirection == KBubblesEntry.DIRECTION_RIGHT) {
+//                                rightMargin = mMargin
+//                            } else if (mDirection == KBubblesEntry.DIRECTION_TOP) {
+//                                topMargin = mMargin
+//                            } else if (mDirection == KBubblesEntry.DIRECTION_BOTTOM) {
+//                                bottomMargin = mMargin
+//                            }
 //                            setAutoPaddingForRadius(kpx.x(16), this)//fixme 根据radius的外补丁，自动设置文本内补丁。
 //                        }
 //                        radius_press {
-//                            bg_color = Color.parseColor("#00A2CA")
+//                            bg_color = pressColor
 //                            strokeColor = Color.RED
 //                            strokeWidth = kpx.x(3f)
+//                            dashWidth=kpx.x(15f)
+//                            dashGap=kpx.x(10f)
 //                        }
 //                    }.lparams {
-//                        width = kpx.x(300)
-//                        height = kpx.x(150)
+//                        width = kpx.x(400)
+//                        height = kpx.x(100)
 //                    }
 
 /**
@@ -193,43 +220,105 @@ open class K6BubblesWidget : K5LparamWidget {
                 mBubblesPathStroke = Path()
             }
             mBubblesPathStroke?.reset()
+            //顶点坐标
+            var x1 = 0f
+            var y1 = 0f
+            //右边上方点坐标
+            var x2 = 0f
+            var y2 = 0f
+            //右边下方点坐标
+            var x3 = 0f
+            var y3 = 0f
             if (direction == KBubblesEntry.DIRECTION_LEFT) {
-                //左居中气泡
+                //fixme 左居中气泡
                 //左边顶点坐标
-                var x1 = 0f + xOffset
-                var y1 = height / 2 + yOffset + bubblesOffset
+                x1 = 0f + xOffset
+                y1 = height / 2 + yOffset + bubblesOffset
                 //右边上方点坐标
-                var x2 = 0f + xOffset + bubblesWidth
-                var y2 = height / 2 + yOffset - bubblesHeight / 2
+                x2 = 0f + xOffset + bubblesWidth
+                y2 = height / 2 + yOffset - bubblesHeight / 2
                 //右边下方点坐标
-                var x3 = 0f + xOffset + bubblesWidth
-                var y3 = height / 2 + yOffset + bubblesHeight / 2
-                mBubblesPath?.moveTo(x1, y1)
+                x3 = 0f + xOffset + bubblesWidth
+                y3 = height / 2 + yOffset + bubblesHeight / 2
+            } else if (direction == KBubblesEntry.DIRECTION_RIGHT) {
+                //fixme 右居中气泡
+                //顶点
+                x1 = width - xOffset
+                y1 = height / 2 + yOffset + bubblesOffset
+                //点2
+                x2 = width - xOffset - bubblesWidth
+                y2 = height / 2 + yOffset - bubblesHeight / 2
+                //点3
+                x3 = width - xOffset - bubblesWidth
+                y3 = height / 2 + yOffset + bubblesHeight / 2
+            } else if (direction == KBubblesEntry.DIRECTION_TOP) {
+                //fixme 上居中气泡
+                //顶点
+                x1 = width / 2 + xOffset + bubblesOffset
+                y1 = 0 + yOffset
+                //点2
+                x2 = width / 2 + xOffset - bubblesWidth / 2
+                y2 = y1 + bubblesHeight
+                //点3
+                x3 = width / 2 + xOffset + bubblesWidth / 2
+                y3 = y2
+            } else if (direction == KBubblesEntry.DIRECTION_BOTTOM) {
+                //fixme 下居中气泡
+                //顶点
+                x1 = width / 2 + xOffset + bubblesOffset
+                y1 = height - yOffset
+                //点2
+                x2 = width / 2 + xOffset - bubblesWidth / 2
+                y2 = y1 - bubblesHeight
+                //点3
+                x3 = width / 2 + xOffset + bubblesWidth / 2
+                y3 = y2
+            }
+            //点坐标连线
+            mBubblesPath?.moveTo(x1, y1)
+            mBubblesPath?.lineTo(x2, y2)
+            if (all_radius != 0f) {
                 mBubblesPath?.lineTo(x2, y2)
-                if (all_radius != 0f) {
-                    mBubblesPath?.lineTo(x2, y2)
-                }
+            }
+            mBubblesPath?.lineTo(x3, y3)
+            if (all_radius != 0f) {
                 mBubblesPath?.lineTo(x3, y3)
+            }
+            mBubblesPath?.close()
+            //边框点坐标连线
+            if (strokeWidth > 0) {
+                var strokeOffset = strokeWidth
+                if (!isDrawRightStroke) {
+                    //右边边框不绘制时，右边留点间隙，方便与radius组合对接。
+                    if (direction == KBubblesEntry.DIRECTION_LEFT) {
+                        x2 = x2 - strokeOffset
+                        x3 = x3 - strokeOffset
+                    } else if (direction == KBubblesEntry.DIRECTION_RIGHT) {
+                        x2 = x2 + strokeOffset
+                        x3 = x3 + strokeOffset
+                    } else if (direction == KBubblesEntry.DIRECTION_TOP) {
+                        y2 = y2 - strokeOffset
+                        y3 = y3 - strokeOffset
+                    } else if (direction == KBubblesEntry.DIRECTION_BOTTOM) {
+                        y2 = y2 + strokeOffset
+                        y3 = y3 + strokeOffset
+                    }
+                }
+                mBubblesPathStroke?.moveTo(x1, y1)
+                mBubblesPathStroke?.lineTo(x2, y2)
                 if (all_radius != 0f) {
-                    mBubblesPath?.lineTo(x3, y3)
-                }
-                mBubblesPath?.close()
-                //边框
-                if (strokeWidth > 0) {
-                    var offset = strokeWidth
-                    x2 = x2 - offset
-                    x3 = x3 - offset
-                    mBubblesPathStroke?.moveTo(x1, y1)
                     mBubblesPathStroke?.lineTo(x2, y2)
-                    if (all_radius != 0f) {
-                        mBubblesPathStroke?.lineTo(x2, y2)
-                    }
-                    mBubblesPathStroke?.moveTo(x3, y3)//fixme 画边框，最右边的边框就不画了。
-                    if (all_radius != 0f) {
-                        mBubblesPathStroke?.lineTo(x3, y3)
-                    }
-                    mBubblesPathStroke?.lineTo(x1, y1)
                 }
+                if (isDrawRightStroke) {
+                    //绘制右边边框
+                    mBubblesPathStroke?.lineTo(x3, y3)
+                } else {
+                    mBubblesPathStroke?.moveTo(x3, y3)//fixme 画边框，最右边的边框就不画了。
+                }
+                if (all_radius != 0f) {
+                    mBubblesPathStroke?.lineTo(x3, y3)
+                }
+                mBubblesPathStroke?.lineTo(x1, y1)
             }
             //圆角
             var corner: CornerPathEffect? = null
